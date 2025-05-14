2209443b9f608f330f015a588d93bd6d3ab30054
Ashahab007 <ashahab007@gmail.com>
2025-05-15 01:33:29 +0600

    added delete button with sweet alert without delete functionality 8.3

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const { photoURL, price, Quantity, name } = coffee;
+  // 8.0 my requirement is delete coffee items upon delete showing with sweet alert with double confirm.
+  const { _id, photoURL, price, Quantity, name } = coffee;
+
+  // 8.3 created handleDelete function with sweet alert
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      console.log(result.isConfirmed);
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className="card card-side bg-base-100 shadow-sm">
@@ -14,10 +39,16 @@ const CoffeeCard = ({ coffee }) => {
           <p>Quantity: {Quantity}</p>
           <p>Price: {price}</p>
           <div className="card-actions justify-end">
-            <div className="join join-vertical">
-              <button className="btn join-item">Button</button>
-              <button className="btn join-item">Button</button>
-              <button className="btn join-item">Button</button>
+            <div className="join join-vertical space-y-2">
+              <button className="btn join-item">View </button>
+              <button className="btn join-item">Edit</button>
+              {/* 8.1 getting the item by its id */}
+              <button
+                onClick={() => handleDelete(_id)}
+                className="btn join-item"
+              >
+                X
+              </button>
             </div>
           </div>
         </div>
